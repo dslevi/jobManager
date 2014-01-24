eb33a433121b0948085a048f061bb6af13f45f2d
Danielle Levi <dslevi12@gmail.com>
2014-01-23 22:28:35 -0800

    Adding basic frontend framework

--- controller.py
+++ controller.py
@@ -7,7 +7,7 @@ app.config.from_object(config)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/<taskId>")
 def displayDetails(taskId):
