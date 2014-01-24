dd5ec5d43607f740ae2bd3e6d659117b87502ac0
Danielle Levi <dslevi12@gmail.com>
2014-01-23 23:03:21 -0800

    working on login/registration

--- controller.py
+++ controller.py
@@ -5,23 +5,60 @@ import config
 app = Flask(__name__)
 app.config.from_object(config)
 
+@app.route("/")
+def index():
+    if session.get('userId'):
+        return render_template("home.html")
+    return redirect(url_for("login"))
+
 @app.route("/login")
 def login():
     return render_template("login.html")
 
-@app.route("/")
-def index():
-    return render_template("home.html")
+@app.route("/validate", methods=["POST"])
+def validate():
+    email = request.forms.get('email')
+    password = request.forms.get('password')
+    user = User.query.filter_by(email=email)
+    if user.authenticate(password):
+        session['userId'] = user.id
+        return redirect(url_for('index'))
+    else:
+        flash("Invalid username or password")
+        return redirect(url_for('login'))
 
-@app.route("/<taskId>")
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/registerUser", methods=["POST"])
+def registerUser():
+    email = request.forms.get('email')
+    password = request.forms.get('password')
+    verify = request.forms.get('verify')
+    if successful:
+        return redirect(url_for("login"))
+    flash("Invalid information")
+    return redirect(url_for("register"))
+
+@app.route("/companies")
+def companies():
+    return render_template("companies.html")
+
+@app.route("/schedule")
+def schedule():
+    return render_template("schedule.html")
+
+@app.route("/help")
+def help():
+    return render_template("help.html")
+
+@app.route("/task/<taskId>")
 def displayDetails(taskId):
     # task = UserTake.query.get(taskId)
     task = "<form><input type='text'></input></form>"
     return render_template("taskDetails.html", task=task)
 
-@app.route("/hello")
-def test():
-    return render_template("test.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
